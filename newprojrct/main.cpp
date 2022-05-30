d08dfdba3114d6b0f7947c099dc03d1ae2bbc57a
ebrahim <ebrahimzeinali1375@gmail.com>
2022-05-30 10:28:12 +0200

    Initial Commit

---
+++ newprojrct/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  newprojrct
+//
+//  Created by ebrahim on 5/30/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
